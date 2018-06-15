35f70bd6c73369a2b84eac9b7cfc79fbe049aab4
Martin Juul <code@juul.xyz>
2018-06-15 10:50:09 +0200

    Initial commit

---
+++ start_containers.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker start freechat-pgadmin
+docker start freechat-db
+docker start freechat-redis
---
+++ stop_containers.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker stop freechat-pgadmin
+docker stop freechat-db
+docker stop freechat-redis
